96ab02d51ee7110bedbd546ca80df8544f811aa1
Alex Wilmer <djazium@gmail.com>
2019-07-24 10:33:44 -0400

    ✨

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import React from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { execute, timeout } from 'effection/src'
+import {restartable} from './restart'
+
+// console.log(123, timeout)
+
+let url = value => `https://api.github.com/search/repositories?q=${value}+language:assembly&sort=stars&order=desc`
+
+let searchGithub = value => execution => {
+  fetch(url(value))
+    .then(r => r.json())
+    .then(json => execution.resume(json))
+    .catch(err => execution.throw(err))
+}
+
+function* search ({ value, setResult }) {
+    console.log(123, value)
+    yield timeout(1000)
+    let result = yield searchGithub(value)
+    setResult(result)
+}
+
+let restartableSearch = restartable(search)
 
 function App() {
+  let [value, setValue] = useState('')
+  let [result, setResult] = useState({})
+
+  // useEffect(() => {    
+    // 
+    
+    // return function () {
+      // restartable.halt()
+    // }
+  // }, [value])
+    
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>autocomplete</h1>
+      <input value={value} onChange={e => {
+        setValue(e.target.value)
+        restartableSearch.perform({ value: e.target.value })
+      }} />
+
+      {JSON.stringify(result)}
     </div>
   );
 }
---
+++ src/restart.js
@@ -0,0 +1,25 @@
+import { execute } from 'effection/src'
+
+export function restartable(proc) {
+  let loop = execute(function*() {
+
+    // state
+    let current = { halt() {} };
+
+    while (true) {
+      let args = yield;
+
+      this.fork(function() {
+        //logic
+        current.halt();
+        current = this.fork(proc, args);
+      });
+    }
+  });
+
+  // performance function
+  return {
+    task: loop,
+    perform: (...args) => loop.resume(...args)
+  }
+}
